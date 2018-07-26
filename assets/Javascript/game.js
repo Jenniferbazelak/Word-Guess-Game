d023833fa5c6057b401e88993efe3cdec74d54e8
Baz <jennigoldstein@gmail.com>
2018-07-26 16:52:40 -0500

    update

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -5,9 +5,8 @@ var wins = 0
 var losses = 0
 var guessesMade = []
 var guessesLeft = 8
-
-//list of words to choose from
 var words = ["beach", "sand", "ocean", "waves", "fish", "sun", "swimsuit", "towel", "seashell"]
+var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 //choose random word from words array
 var randomWord = words[Math.floor(Math.random() * words.length)];
@@ -17,8 +16,9 @@ var randomWord = words[Math.floor(Math.random() * words.length)];
 var answerBlanks = [];
 for (var i = 0; i < randomWord.length; i++) {
     answerBlanks[i] = ("_");
-  //  document.getElementById("currentword").innerHTML = answerBlanks;
 }
+document.getElementById("currentword").textContent = answerBlanks.join("");
+
 
 // define reset function
 function reset() {
@@ -29,45 +29,64 @@ function reset() {
     randomWord = words[Math.floor(Math.random() * words.length)];
     for (var i = 0; i < randomWord.length; i++) {
         answerBlanks[i] = "_";
-        document.getElementById("currentword").innerHTML = answerBlanks;
+        document.getElementById("currentword").innerHTML = answerBlanks.join("");
     }
 }
 
+//define answerBlank function
+
+function updateanswerBlanks(l, index) {
+    answerBlanks.splice(index, 1, l);
+}
+
+//GAME STARTS HERE
 //when user clicks on a letter
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     console.log("Guess: " + userGuess);
     console.log("Correct word: " + randomWord);
 
-    for (var j = 0; j < randomWord.length; j++) {
-        if(randomWord[j] === userGuess) {
-            answerBlanks[j] = userGuess;
-        }
-         else if (randomWord[j] !== userGuess) {
-            guessesMade.push(userGuess);
-            document.getElementById("lettersguessed").innerHTML = guessesMade;
-            guessesLeft--;
-            document.getElementById("guessesremain").innerHTML = guessesLeft;
-            if (guessesLeft === 0) {
-                losses++;
-                document.getElementById("losses").innerHTML = losses;
-                console.log("You LOSE!");
-                reset();
-            }
-        }
+    guessesMade.push(userGuess);
+    document.getElementById("lettersguessed").innerHTML = guessesMade;
+
+    // alert if not a letter
+    if (!letterArray.includes(guess)) {
+         console.log("User pressed: " + userGuess + " Not a letter.");
+        alert("Please press a letter, not a special key.");
+        return;
     }
 
-            // //Game over
-            // function gameOver() {
-            //     if (letterBlanks.indexOf("_") === -1) {
-            //         wins++;
-            //         alert("YA YOU WON!");
-            //     } else {
-            //         losses++;
-            //         alert("SO SAD... YOU LOST")
-            //     }
+    // alert if already picked that letter
+    if (guessesMade.includes(userGuess)) {
+        console.log(userGuess + " was already guessed.");
+        alert("You already guessed " + userGuess + ". Pick another letter.")
+        return;
+    }
 
 
-    
-            // }
+    //update the answerBlanks for current word  
+    for (var j = 0; j < randomWord.length; j++) {
+        if (randomWord[j] === userGuess) {
+            updateanswerBlanks(userGuess, j);
+        }
+    }
+    if (!answerBlanks.includes("_")) {
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        console.log("You WIN!");
+    }
+
+    else if (randomWord[j] !== userGuess) {
+        guessesLeft--;
+        document.getElementById("guessesremain").innerHTML = guessesLeft;
+        if (guessesLeft === 0) {
+            losses++;
+            document.getElementById("losses").innerHTML = losses;
+            console.log("You LOSE!");
+            reset()
+        }
+    }
 }
+
+
+
